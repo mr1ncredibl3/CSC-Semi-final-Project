77441c58a752d482693618d91de211de98b6def4
mr1ncredibl3 <abdoahmedabdelsameea@gmail.com>
2025-08-09 16:56:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function toggleSidebar() {
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show');
+        ul.previousElementSibling.classList.remove('rotate');
+    });
+}
+
+function toggleSubMenu(button){
+    button.nextElementSibling.classList.toggle('show');
+    button.classList.toggle('rotate');
+    if(sidebar.classList.contains('close')) {
+        sidebar.classList.remove('close');
+        toggleButton.classList.remove('rotate');
+    }
+}
